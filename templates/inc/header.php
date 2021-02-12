4a3d62abf21cae901345f6267de90df70c9cdd11
viktor-batashki <67549433+viktor-batashki@users.noreply.github.com>
2021-02-12 08:17:19 +0200

    Create header.php

---
+++ templates/inc/header.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Monster HR</title>
+        <link rel="stylesheet" href="https://bootswatch.com/4/flatly/bootstrap.min.css"/>
+        <link rel="stylesheet" href="css/style.css"/>
+    </head>
+    <body>
+         <div class="container">
+            <div class="header clearfix">
+              <nav>
+                <ul class="nav nav-pills pull-right">
+                  <li role="presentation" class="active"><a href="index.php">Home</a></li>
+                  <li role="presentation"><a href="create.php">Create listing</a></li>
+                </ul>
+              </nav>
+              <h3 class="text-muted">Monster HR</h3>
+            </div>
+             <?php displayMessage(); ?>
